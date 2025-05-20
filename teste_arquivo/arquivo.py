464b7dd4cf0e15c9cc4ee0c30af2b06f24e10b92
Nathalia-Valentim <nathadelino@hotmail.com>
2025-05-20 18:12:34 -0300

    import os

--- teste_arquivo/arquivo.py
+++ teste_arquivo/arquivo.py
@@ -2,6 +2,7 @@
 with open("nome_arquivo.txt", "w") as arquivotexto:
     arquivotexto.write("Qualquer texto dentro do arquivo")
 '''
+import os
 
 with open("comandos.txt", "r") as meuarquivo:
     conteudo_arquivo = meuarquivo.read()
